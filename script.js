b9ff62f504f839fcc9b7e66ad083d363bdf6ca75
vivianluh <s3900746@student.rmit.edu.au>
2022-08-19 10:57:04 +1000

    net art initialised and working

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log (document)
+// document.body.bgColor = "turquoise"
+
+document.body.style.margin   = 0
+document.body.style.overflow = `hidden`
+
+function setup () {
+    createCanvas (window.innerWidth, window.innerHeight)
+}
+
+function draw () {
+    background (`turquoise`)
+}
